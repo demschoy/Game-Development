922d17d4ff347edb7e5c0da4646bdbc638d7cc21
Demschoy <demschoy@gmail.com>
2020-03-25 11:28:59 +0200

    Upload homework

---
+++ Homeworks/Homework 1/Unity Basics/Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    [SerializeField]
+    private Transform target = null;
+    private Vector3 offset;
+    [SerializeField]
+    private float smoothedSpeed = 15;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = transform.position - target.position;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector3 position = target.position + offset;
+        transform.position = Vector3.Lerp(transform.position, position, smoothedSpeed * Time.deltaTime);
+        transform.LookAt(target.position);
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Game-Development
+
+This repository contains materials for Game Development course at FMI.
