f1f6506a6805749eb3c5151df55713f67403d8c1
Demschoy <demschoy@gmail.com>
2020-03-27 16:20:50 +0200

    Improved camera following object visibility

--- Homeworks/Homework 1/Unity Basics/Assets/Scripts/CameraMovement.cs
+++ Homeworks/Homework 1/Unity Basics/Assets/Scripts/CameraMovement.cs
@@ -9,7 +9,7 @@ public class CameraMovement : MonoBehaviour
     [SerializeField]
     private Vector3 offset = new Vector3(0, 0, 0);
     [SerializeField]
-    private float smoothedSpeed = 15;
+    private float smoothedSpeed = 8;
 
     // Start is called before the first frame update
     void Start()
