f20693b663e56c1ae00338e910680fd94a8bf738
Demschoy <demschoy@gmail.com>
2020-03-28 16:08:18 +0200

    Add second homework task

---
+++ Homeworks/Homework 2/Physics/Assets/Scripts/PlayerJump.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerJump : MonoBehaviour
+{
+    private Rigidbody body;
+    [SerializeField]
+    private float force = 2;
+    private bool isGrounded = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        body = GetComponent<Rigidbody>();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("Plane"))
+        {
+            isGrounded = true;
+        } 
+        else
+        {
+            isGrounded = false;
+        }
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Plane"))
+        {
+            isGrounded = true;
+        }
+        else
+        {
+            isGrounded = false;
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Plane"))
+        {
+            isGrounded = false;
+        }
+        else
+        {
+            isGrounded = true;
+        }
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (isGrounded)
+        {
+            body.useGravity = false;
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                body.AddForce(Vector3.up * force, ForceMode.Impulse);
+            }
+        }
+        body.useGravity = true;
+    }
+}
