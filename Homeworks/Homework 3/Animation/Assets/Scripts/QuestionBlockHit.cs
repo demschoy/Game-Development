9fe811946e7149c65d5295b6822408d74d4908c2
Demschoy <demschoy@gmail.com>
2020-04-03 18:00:04 +0300

    Add basic homework 3. TODO - cosmetic transformation details from Small Mario to Big Mario, and walking on blocks.

---
+++ Homeworks/Homework 3/Animation/Assets/Scripts/QuestionBlockHit.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuestionBlockHit : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject mushroom = null;
+    private bool isHit = false;
+    private Animator mushroomAnimator;
+    private Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        mushroomAnimator = mushroom.GetComponent<Animator>();
+        mushroom.SetActive(false);  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.CompareTag("Player"))
+        {
+            if(!isHit)
+            {
+                isHit = true;
+                animator.SetBool("IsHit", isHit);
+                mushroom.SetActive(true);
+                mushroomAnimator.SetBool("IsVisible", true);
+            }
+        }
+    }
+}
